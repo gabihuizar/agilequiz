b90b734d3e4cbd16b0084c84421cf32554275ae3
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2015-12-01 17:08:20 +0000

    changed dogs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201014823) do
+ActiveRecord::Schema.define(version: 20151201170452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "dogs", force: true do |t|
-    t.string "name"
+    t.string  "name"
+    t.string  "color"
+    t.integer "age"
   end
 
 end
