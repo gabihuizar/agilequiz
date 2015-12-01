a24e8711555e2472a2973b062a63a355e8b5c752
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2015-12-01 01:43:40 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Agilequiz::Application.config.secret_key_base = 'bf1123b42874f9814bc018f66d5425e9f17ecb60ce7c68673b26896c40a15bed47185e2b4cb380aecc46230477031cade582c11aac793c8277ccb2d434062809'
